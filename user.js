b9413c6f33e243024d2f0f5f4315c478ad72b369
Ferosh <52982404+ferodevs@users.noreply.github.com>
2022-06-23 19:16:36 -0700

    init: theme

---
+++ user.js
@@ -0,0 +1,9 @@
+// These will edit your about:config to enable properties that ensure the theme works properly.
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.proton.enabled", true);
+user_pref("browser.proton.places-tooltip.enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("layout.css.backdrop-filter.enabled", true);
+user_pref("browser.compactmode.show", true);
